35fe8bc96058bd6bcab386d67c0461feaf8c95fb
funa-1222 <funabashi-ayaka@gmail.com>
2023-03-08 19:28:25 +0900

    入力画面　js追加

---
+++ server/views/js/index.js
@@ -0,0 +1,8 @@
+/*-----index.ejs-----*/
+/*入力ボタンを押したらデータを更新する*/
+// document.getElementById('input-button').addEventListener('click', 
+// function() {
+        //if　商品名, 金額, 賞味・消費期限の部分がどれか空白だったら
+            //alert
+//     //データベースにデータを更新
+// })
