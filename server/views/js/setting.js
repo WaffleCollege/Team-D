f7ec62c665521ee8202ffea6cd3ee636bd4fe0fb
funa-1222 <funabashi-ayaka@gmail.com>
2023-03-08 19:45:35 +0900

    setting ボタンイベント　コメントだけ

---
+++ server/views/js/setting.js
@@ -0,0 +1,10 @@
+/*-----setting.ejs-----*/
+/*保存ボタンを押したらデータを更新する*/
+// document.getElementById('save-button').addEventListener('click', 
+// function() {
+        //if　食費目標, 賞味・消費期限, 金額単位の部分がどれか空白
+            //alert 「空白の部(ex:食費/月)」を入力してください
+        //if 通知ありのかつ何日前かのところが空白
+            //alert　「通知設定を入力してください」
+//     //データベースにデータを更新
+// })
