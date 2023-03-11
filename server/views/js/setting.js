c452e5625660d31281ebfd7ef49b648aa2c6b91a
funa-1222 <funabashi-ayaka@gmail.com>
2023-03-11 17:20:13 +0900

    賞味・消費期限を日付選択できるように変更

--- server/views/js/setting.js
+++ server/views/js/setting.js
@@ -2,7 +2,7 @@
 /*保存ボタンを押したらデータを更新する*/
 // document.getElementById('save-button').addEventListener('click', 
 // function() {
-        //if　食費目標, 賞味・消費期限, 金額単位の部分がどれか空白
+        //if　食費目標, 金額単位の部分がどれか空白
             //alert 「空白の部(ex:食費/月)」を入力してください
         //if 通知ありのかつ何日前かのところが空白
             //alert　「通知設定を入力してください」
